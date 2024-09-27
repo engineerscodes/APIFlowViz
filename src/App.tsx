7a32c08886d1967beca125a2e02be3d0f3c6b250
engineerscodes <practicalengineers05@gmail.com>
2024-09-27 22:01:03 +0530

    setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import AppBtn from './AppBtn'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+<AppBtn></AppBtn>
+    </>
+  )
+}
+
+export default App

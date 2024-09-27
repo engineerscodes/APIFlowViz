0ee9d05408779d9385003db11a0d05e6dbdebbb5
engineerscodes <practicalengineers05@gmail.com>
2024-09-27 23:34:26 +0530

    added react flow

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-<AppBtn></AppBtn>
+      <AppBtn></AppBtn>
     </>
   )
 }
--- src/AppBtn.tsx
+++ src/AppBtn.tsx
@@ -1,9 +1,45 @@
-import { Button } from "@/components/ui/button"
-
-export default function AppBtn() {
+import {
+  ReactFlow,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  BackgroundVariant,
+  MarkerType,
+} from '@xyflow/react';
+ 
+import '@xyflow/react/dist/style.css';
+import { useCallback } from 'react';
+ 
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } , type: 'bidirectional'},
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' }, type: 'bidirectional' },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' ,markerEnd: { type: MarkerType.ArrowClosed }}];
+ 
+export default function App() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+ 
+  const onConnect = useCallback(
+    (params: any) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+  );
   return (
-    <div>
-      <Button>Click me</Button>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Controls />
+        <MiniMap />
+        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+      </ReactFlow>
     </div>
-  )
+  );
 }
